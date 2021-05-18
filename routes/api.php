3c1315b5b0dcf8f1082c78648576804516afdc0d
abram7-lacetania <82396295+abram7-lacetania@users.noreply.github.com>
2021-05-18 16:09:19 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json($request->user(),200);
 });
 Route::middleware('auth:sanctum')->get('/athenticated', function () {
     return true;
